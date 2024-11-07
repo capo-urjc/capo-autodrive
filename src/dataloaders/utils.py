4c5c82d179913e1b60bdb3a45a2019ad223e33cb
SergioHdezG <sergio.hernandez@urjc.es>
2024-11-07 18:26:51 +0000

    src/dataloader/utils.py added.

---
+++ src/dataloaders/utils.py
@@ -0,0 +1,16 @@
+# Sensor name code
+
+sensor_code = {
+    'rgb_f':    'CameraForward',
+    'rgb_lf':   'CameraLeftForward',
+    'rgb_lb':   'CameraLeftBackward',
+    'rgb_b':    'CameraBackward',
+    'rgb_rb':   'CameraRightBackward',
+    'rgb_rf':   'CameraRightForward',
+    'rgb_bev':  'CameraBEV',
+    'gnss':     'GnssTest',
+    'imu':      'IMUTest',
+    'lidar':    'LidarTest',
+    'radar':    'RADARTest'
+
+}
